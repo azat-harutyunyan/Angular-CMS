2231d4557441de0c53f7e7ab1246bf38bd560ac3
Azat <harutyunyanazat29@gmail.com>
2024-03-21 21:30:03 +0400

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+	production: false,
+	firebase: {
+		apiKey: "AIzaSyCBQ26hyrsPIxJtN1MwAPycnBnqvaYAy9U",
+		authDomain: "cms-angulario.firebaseapp.com",
+		databaseURL: "https://cms-angulario-default-rtdb.firebaseio.com",
+		projectId: "cms-angulario",
+		storageBucket: "cms-angulario.appspot.com",
+		messagingSenderId: "911101178041",
+		appId: "1:911101178041:web:ebaf0abf3170e5962eab68",
+		measurementId: "G-S34NPRN8E4"
+	}
+}
