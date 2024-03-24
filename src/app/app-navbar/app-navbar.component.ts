9738da119d3c15070e16218b77c6e6eff3fa23af
Azat <azat.harutyunyan.fd@gmail.com>
2024-03-24 17:13:01 +0400

    Add Strcuture with Material Design

--- src/app/app-navbar/app-navbar.component.ts
+++ src/app/app-navbar/app-navbar.component.ts
@@ -3,11 +3,15 @@ import { AfService } from '../providers/af.service';
 import { User } from '../providers/user';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatButton, MatIconModule, MatMenuModule],
   templateUrl: './app-navbar.component.html',
   styleUrl: './app-navbar.component.css'
 })
