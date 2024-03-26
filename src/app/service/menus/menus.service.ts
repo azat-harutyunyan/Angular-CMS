0c07baa6ce60613a76e48525a19ca545a0ecb35e
Azat <azat.harutyunyan.fd@gmail.com>
2024-03-26 11:47:31 +0400

    Add menus service

---
+++ src/app/service/menus/menus.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs';
+
+export interface Menu {
+  title: string,
+  url: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenusService {
+
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  getMenus() {
+    return this.afs.collection("menus").snapshotChanges()
+      .pipe(
+        map(menu => {
+          return menu.map(a => {
+            const data = a.payload.doc.data() as Menu
+            const id = a.payload.doc.id
+            return { id, ...data }
+          })
+        })
+      )
+
+  }
+
+  addMenu(menu: Menu) {
+    this.afs.collection("menus").add(menu)
+  }
+
+  deleteMenu(menuId: number) {
+    this.afs.doc('menus/' + menuId).delete()
+  }
+
+  updateMenu(menuId: number, menu: Menu) {
+    this.afs.doc('menus/' + menuId).update(menu)
+  }
+}
